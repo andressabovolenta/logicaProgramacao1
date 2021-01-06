f9f7ddf2fb5f263fa14596bb00ae089bda8ac48f
Andressa Bovolenta <andressa.bbovolenta@gmail.com>
2021-01-06 16:54:34 -0300

    first commit

---
+++ LogicaProgramacao1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace LogicaProgramacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //APRENDENDO A DECLARAR VARIÁVEIS:
+            //string hello = "Seja bem-vindo à nossa aula!";
+            //int inteiro = 1;
+            //double numero = 1.5;
+            //bool booleano = false;
+            //Console.WriteLine(hello);
+
+            //ARRAYS:
+            //int[] numeros = new int[5];
+            //numeros[0] = 1;
+            //numeros[1] = 25;
+            //numeros[2] = 50;
+            //numeros[3] = 100;
+            //numeros[4] = 125;
+            //Console.WriteLine(numeros[3]);
+
+            //EXPRESSÕES LÓGICAS e CONSTANTES:
+            //const int constInteiro = 20;
+            //int inteiro1 = 10;
+            //int inteiro2 = 20;
+            //int soma = inteiro1 + inteiro2;
+            //Console.WriteLine(soma);
+            //inteiro1 = 50 + soma;
+            //Console.WriteLine(inteiro1 * 2);
+
+            //CONCATENAÇÃO:
+            //string bemVindo = "Seja bem-vinda, ";
+            //string nome = "Andressa!";
+            //Console.WriteLine(frase);
+            //OU
+            //Console.WriteLine(bemVindo + nome);
+            //OU
+            //string nome = "Andressa";
+            //string idade = "30";
+            //string bemVindo = $"Seja bem-vinda, {nome}. Sua idade é de {idade} anos.";
+            //Console.WriteLine(bemVindo);
+
+            //OPERADORES RELACIONAIS
+            //bool boolean = false;
+            //if (boolean == true)
+            //{
+            //    Console.WriteLine("A condição é verdadeira!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A condição é falsa!");
+
+            //int numero = 10;
+            //if (numero <= 10)
+            //{
+            //    Console.WriteLine("A condição é verdadeira!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A condição é falsa!");
+            //}
+
+            //int numero = 10;
+            //if (numero != 10)
+            //{
+            //    Console.WriteLine("A condição é verdadeira!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A condição é falsa!");
+            //}
+
+            //OPERADORES LÓGICOS && || !
+            //int numero = 100;
+            //bool boolean = true;
+            //if (numero != 10 && boolean == true)
+            //{
+            //    Console.WriteLine("A condição é verdadeira!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A condição é falsa!");
+            //}
+
+            //string sobremesa = "Pudim";
+            //bool estouAcimaDoPeso = false;
+
+            //if (estouAcimaDoPeso || sobremesa == "Pudim")
+            //{
+            //    Console.WriteLine("Comer sobremesa!");
+            //}
+
+            //bool termineiTrabalho = false;
+            //bool estouComfome = true;
+
+            //if (!termineiTrabalho && estouComfome)
+            //{
+            //    Console.WriteLine("Almoçar!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Continuar trabalhando!");
+            //}
+
+            //bool souRico = false;
+            //double salario = 200000;
+
+            //if (souRico || salario >= 50000)
+            //{
+            //    Console.WriteLine("Você é rico!");
+            //}
+
+        }
+    }
+}
